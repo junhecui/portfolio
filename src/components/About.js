8e54a5547cee3a660adbb3cd36f287cb515a2dcf
junhe-c <cjunhe05@gmail.com>
2024-06-11 22:11:46 +0800

    description and added deploy

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => (
   <section id="about" className="py-16 bg-primary">
     <div className="container mx-auto">
       <h2 className="text-3xl font-bold mb-4">About Me</h2>
-      <p className="text-lg">Short bio goes here.</p>
+      <p className="text-lg">I'm full stack web developer and a third-year Computer Science student at UBC! My current interests include Natural Language Processing, Machine Learning, and Unity Game Development; in my free time, I am likely bouldering, surfing, snowboarding, or listening to some music! </p>
     </div>
   </section>
 );
